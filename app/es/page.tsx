7ba9fe394de0fb2be10aa44a72341b74756b3730
rortan134 <samaritanojr006@gmail.com>
2024-06-23 20:04:07 +0200

    styles

--- app/es/page.tsx
+++ app/es/page.tsx
@@ -2,7 +2,7 @@ import { Link } from "@/navigation";
 import prisma from "@/prisma";
 import { LanguageSelector } from "@components/language-selector";
 import { PageShell } from "@components/page-shell";
-import { dayjs } from "@lib/dayjs";
+import { Timezone } from "@components/timezone";
 import {
     ArrowUpRight,
     Asterisk,
@@ -156,50 +156,7 @@ export default function HomePage() {
                     </Link>
                 </div>
             </section>
-            <section className="container mt-16 grid w-full gap-6 md:grid-cols-2">
-                <div className="flex w-full flex-col space-y-2">
-                    <h4 className="text-sm text-foreground">Your timezone</h4>
-                    <div className="flex items-center justify-between rounded-xl border p-3">
-                        <div className="flex flex-col space-y-1">
-                            <span className="text-xs font-medium text-muted-foreground">
-                                {dayjs.tz.guess()}
-                            </span>
-                            <span className="text-xs text-muted-foreground/75">
-                                (GMT {dayjs().tz(dayjs.tz.guess()).format("Z")})
-                            </span>
-                        </div>
-                        <div className="flex flex-col space-y-0.5">
-                            <span className="text-xs font-medium text-muted-foreground/90">
-                                {dayjs().tz(dayjs.tz.guess()).format("h:mm A")}
-                            </span>
-                            <span className="text-xs text-muted-foreground/75">
-                                {dayjs().tz(dayjs.tz.guess()).format("ddd, MMM DD, YYYY")}
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full flex-col space-y-2">
-                    <h4 className="text-sm text-foreground">My timezone</h4>
-                    <div className="flex items-center justify-between rounded-xl border p-3">
-                        <div className="flex flex-col space-y-1">
-                            <span className="text-xs font-medium text-muted-foreground">
-                                Europe/Madrid
-                            </span>
-                            <span className="text-xs text-muted-foreground/75">
-                                (GMT {dayjs().tz("Europe/Madrid").format("Z")})
-                            </span>
-                        </div>
-                        <div className="flex flex-col space-y-0.5">
-                            <span className="text-xs font-medium text-muted-foreground/90">
-                                {dayjs().tz("Europe/Madrid").format("h:mm A")}
-                            </span>
-                            <span className="text-xs text-muted-foreground/75">
-                                {dayjs().tz("Europe/Madrid").format("ddd, MMM DD, YYYY")}
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <Timezone />
             <footer className="container mt-16 flex flex-row items-center justify-between">
                 <span className="text-[10px] text-muted-foreground/60">
                     @ {new Date().getUTCFullYear()} GSMT. All rights reserved.
