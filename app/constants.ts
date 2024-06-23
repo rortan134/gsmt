fdf5ef58ec7a71df09154db03ae2ae1075a60c8a
rortan134 <samaritanojr006@gmail.com>
2024-06-23 21:38:32 +0200

    stykes

---
+++ app/constants.ts
@@ -0,0 +1,13 @@
+export const projects = [
+    {
+        name: "Project",
+        description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo laborum veniam at nostrum.",
+        tech: ["TypeScript", "Next.js", "Tailwind CSS", "Vercel"],
+        image: "/",
+    },
+];
+
+export const reads = [
+    
+]
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,16 @@ import {
 } from "@components/drawer";
 import { MessageInput } from "@components/message-input";
 import { cn } from "@lib/cn";
-import { ExternalLink, Home, MessageCircle } from "lucide-react";
+import { slugify } from "@lib/slugify";
+import { Home, MessageCircle } from "lucide-react";
 import type { Metadata, Viewport } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import { projects } from "./constants";
 
+// TODO: make static
 export const dynamic = "force-dynamic";
 
 const WEBSITE_URL = "https://gsmt.link";
@@ -62,9 +66,10 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
     const messages = await getMessages();
+
     return (
         <html lang="en" dir="ltr">
-            <body className={cn("pb-20", inter.className)}>
+            <body className={cn("pb-32", inter.className)}>
                 <NextIntlClientProvider messages={messages}>
                     {children}
                     <FloatingNavigation />
@@ -74,105 +79,62 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     );
 }
 
-function FloatingNavigation() {
-    return (
-        <nav className="fixed inset-x-0 bottom-0 pb-12">
-            <div className="absolute bottom-3 left-1/2 flex w-64 -translate-x-1/2 items-center justify-between overflow-hidden rounded-3xl border bg-neutral-300/75 px-1 py-0.5 shadow-sm backdrop-blur-md">
-                <Link
-                    href="/"
-                    title="Home"
-                    className="inline-flex w-full items-center justify-center rounded-2xl bg-neutral-400/60 px-3 py-1.5">
-                    <Home className="h-4 w-4" />
-                    <span className="sr-only">Go to home</span>
-                </Link>
-                <Drawer>
-                    <DrawerTrigger
-                        title="Featured projects"
-                        className="inline-flex w-full items-center justify-center rounded-2xl px-5 py-1.5 text-sm font-medium">
-                        Work
-                        <span className="sr-only">Expand work drawer</span>
-                    </DrawerTrigger>
-                    <DrawerContent>
-                        <DrawerTitle>Work</DrawerTitle>
-                        <DrawerDescription className="max-w-lg px-4">
-                            My focus these days is mainly on the web ecosystem, but I have worked
-                            with many languages and platforms.{" "}
-                            <span className="opacity-50">—&nbsp;Click on a project to expand</span>
-                        </DrawerDescription>
-                        <div className="mt-6 flex w-full flex-col space-y-4 md:space-y-16">
-                            <div className="grid grid-cols-4 gap-4 md:grid-cols-1 md:gap-x-0">
-                                <div className="relative col-span-1 flex flex-col overflow-hidden md:col-span-full md:row-span-full md:w-full md:py-4">
-                                    <div className="absolute inset-0 hidden bg-neutral-200 object-cover md:block"></div>
-                                    <div className="z-10 ml-4 aspect-square h-full max-h-20 w-auto max-w-[80px] rounded-md bg-neutral-300" />
-                                </div>
-                                <div className="col-span-3 flex w-full flex-col space-y-0.5 pr-4 md:pl-4">
-                                    <h3 className="text-sm font-medium text-foreground">Project</h3>
-                                    <p className="text-xs text-muted-foreground">
-                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                                        Quo laborum veniam at nostrum quia tenetur magnam quasi
-                                        vitae.
-                                    </p>
-                                    <div className="flex items-center space-x-4">
-                                        <span className="truncate text-xs text-muted-foreground">
-                                            Lorem • ipsum • dolor
-                                        </span>
-                                        <Link
-                                            href="/"
-                                            className="flex items-center justify-center text-xs text-muted-foreground underline-offset-2 active:underline md:hover:underline">
-                                            View live
-                                            <ExternalLink className="ml-1 h-3 w-3" />
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="mt-6 flex w-full flex-col space-y-4 md:space-y-16">
-                            <div className="grid grid-cols-4 gap-4 md:grid-cols-1 md:gap-x-0">
-                                <div className="relative col-span-1 flex flex-col overflow-hidden md:col-span-full md:row-span-full md:w-full md:py-4">
-                                    <div className="absolute inset-0 hidden bg-neutral-200 object-cover md:block"></div>
-                                    <div className="z-10 ml-4 aspect-square h-full max-h-20 w-auto max-w-[80px] rounded-md bg-neutral-300" />
-                                </div>
-                                <div className="col-span-3 flex w-full flex-col space-y-0.5 pr-4 md:pl-4">
-                                    <h3 className="text-sm font-medium text-foreground">Project</h3>
-                                    <p className="text-xs text-muted-foreground">
-                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                                        Quo laborum veniam at nostrum quia tenetur magnam quasi
-                                        vitae.
-                                    </p>
-                                    <div className="flex items-center space-x-4">
-                                        <span className="truncate text-xs text-muted-foreground">
-                                            Lorem • ipsum • dolor
-                                        </span>
-                                        <Link
-                                            href="/"
-                                            className="flex items-center justify-center text-xs text-muted-foreground underline-offset-2 active:underline md:hover:underline">
-                                            View live
-                                            <ExternalLink className="ml-1 h-3 w-3" />
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </DrawerContent>
-                </Drawer>
-                <Link
-                    href="/reads"
-                    title="Recommended reads"
+const FloatingNavigation = () => (
+    <nav className="fixed inset-x-0 bottom-0 pb-12">
+        <div className="absolute bottom-3 left-1/2 flex w-64 -translate-x-1/2 items-center justify-between overflow-hidden rounded-3xl border bg-neutral-300/75 px-1 py-0.5 shadow-sm backdrop-blur-md">
+            <Link
+                href="/"
+                title="Home"
+                className="inline-flex w-full items-center justify-center rounded-2xl bg-neutral-400/60 px-3 py-1.5">
+                <Home className="h-4 w-4" />
+                <span className="sr-only">Go to home</span>
+            </Link>
+            <Drawer>
+                <DrawerTrigger
+                    title="Featured projects"
                     className="inline-flex w-full items-center justify-center rounded-2xl px-5 py-1.5 text-sm font-medium">
-                    Reads
-                </Link>
-                <Drawer>
-                    <DrawerTrigger
-                        title="Send a Message"
-                        className="inline-flex w-full items-center justify-center rounded-2xl px-3 py-1.5">
-                        <MessageCircle className="h-4 w-4" />
-                        <span className="sr-only">Send a Message</span>
-                    </DrawerTrigger>
-                    <DrawerContent>
-                        <MessageInput />
-                    </DrawerContent>
-                </Drawer>
-            </div>
-        </nav>
-    );
-}
+                    Work
+                    <span className="sr-only">Expand work drawer</span>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerTitle>Work</DrawerTitle>
+                    <DrawerDescription className="max-w-lg px-4">
+                        My focus these days is mainly on the web ecosystem, but I have worked with
+                        many languages and platforms.{" "}
+                        <span className="opacity-50">—&nbsp;Click on a project to expand</span>
+                    </DrawerDescription>
+                    {projects.map(({ name, description, tech, image }, i) => (
+                        <Link
+                            key={i}
+                            href={`/projects/${slugify(name)}`}
+                            className="mt-6 flex w-full flex-col space-y-0.5 pr-4 md:pl-4">
+                            <h3 className="text-sm font-medium text-foreground">{name}</h3>
+                            <p className="text-xs text-muted-foreground">{description}</p>
+                            <span className="truncate text-xs text-muted-foreground">
+                                {tech.join(" • ")}
+                            </span>
+                            <Image src={image} alt="" />
+                        </Link>
+                    ))}
+                </DrawerContent>
+            </Drawer>
+            <Link
+                href="/reads"
+                title="Recommended reads"
+                className="inline-flex w-full items-center justify-center rounded-2xl px-5 py-1.5 text-sm font-medium">
+                Reads
+            </Link>
+            <Drawer>
+                <DrawerTrigger
+                    title="Send a Message"
+                    className="inline-flex w-full items-center justify-center rounded-2xl px-3 py-1.5">
+                    <MessageCircle className="h-4 w-4" />
+                    <span className="sr-only">Send a Message</span>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <MessageInput />
+                </DrawerContent>
+            </Drawer>
+        </div>
+    </nav>
+);
