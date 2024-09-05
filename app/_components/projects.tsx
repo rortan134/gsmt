7986e966bd0eb172bc9b26cb0609bc34e3a3c3cd
rortan134 <samaritanojr006@gmail.com>
2024-09-05 07:03:26 +0200

    style

--- app/_components/projects.tsx
+++ app/_components/projects.tsx
@@ -2,20 +2,24 @@ import { Carousel } from "@components/carousel";
 import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 
+const Card = ({ children }: { children: React.ReactNode }) => (
+    <div className="relative flex aspect-square h-full px-3 py-4 md:px-5 md:py-6">{children}</div>
+);
+
 export const Projects = () => (
     <Carousel>
-        <div className="relative flex aspect-square h-full px-3 py-4 md:px-5 md:py-6">
+        <Card>
             <div className="h-full w-auto object-cover" />
-        </div>
-        <div className="relative flex h-full">
-            <div className="h-full w-auto object-cover px-3 py-4 md:px-5 md:py-6" />
-        </div>
-        <div className="relative flex aspect-square h-full">
-            <div className="h-full w-auto object-cover px-3 py-4 md:px-5 md:py-6" />
-        </div>
-        <div className="relative flex h-full">
+        </Card>
+        <Card>
             <div className="h-full w-auto object-cover" />
-        </div>
+        </Card>
+        <Card>
+            <div className="h-full w-auto object-cover" />
+        </Card>
+        <Card>
+            <div className="h-full w-auto object-cover" />
+        </Card>
         <Link
             href="httsp://x.com/gsmmtt"
             target="_blank"
