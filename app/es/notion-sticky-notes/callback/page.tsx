3faa77ac04aa9022cad7f0ee7028de30c88f2b4d
gsmt <samaritanojr006@gmail.com>
2025-03-01 02:48:03 +0100

    sync

--- app/es/notion-sticky-notes/callback/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Callback({ searchParams: { code } }: { searchParams: { code: string } }) {
-    redirect(`notion-sticky-notes://${code}`);
-}
